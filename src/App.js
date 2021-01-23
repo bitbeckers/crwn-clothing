d9d4fa2f5d2b50c1fcf18817d2256590a22a1236
bitbeckers <code@bitbeckers.com>
2021-01-23 22:18:58 +0100

    added collection preview

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
-import HomePage from "./components/homepage/homepage.component";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h>HatsPage</h>
-    </div>
-  )
-  
-}
-
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop/hats" component={HatsPage} />
-        </Switch>
-      </div>
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
