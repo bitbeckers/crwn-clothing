0d5ea09022f2c3ed563f25a4d8bf33c8b8adcd98
bitbeckers <code@bitbeckers.com>
2021-02-01 17:45:37 +0100

    directory and shop data into reducers, shop and dir components

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,21 @@
+import { createSelector} from 'reselect';
+import memoize from 'lodash.memoize';
+
+const selectShop = state => state.shop;
+
+export const selectCollections = memoize(createSelector(
+    [selectShop],
+    shop => shop.collections
+));
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = collectionUrlParam => {
+    return createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+    )
+}
